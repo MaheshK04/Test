74ecb552420097053c185d4c3cbb6e37df0a368c
MaheshK04 <maheshk04x@gmail.com>
2024-02-17 18:13:46 +0530

    working on local and client

--- server/index.js
+++ server/index.js
@@ -7,16 +7,7 @@ const PORT = 5000;
 const io = require("socket.io")(http, {
   cors: {
     origin: "*",
-  
-    handlePreflightRequest: (req, res) => {
-      res.writeHead(200, {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST",
-        "Access-Control-Allow-Headers": "my-custom-header",
-        "Access-Control-Allow-Credentials": true
-      });
-      res.end();
-  }
+    methods:["GET","POST"]
   },
 });
 
